9d40c97fc5427a34969ed0d983eea787ad08e642
Totemn <163722284+Totenem@users.noreply.github.com>
2024-03-25 20:41:11 +0800

    Update youtube converter.py

--- youtube converter.py
+++ youtube converter.py
@@ -4,7 +4,7 @@ from tkinter import Tk, Canvas, Entry, Button, PhotoImage, messagebox
 from pytube import YouTube
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"C:\Users\khel\OneDrive\Desktop\Programs\Python\build\assets\frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"Enter the path for assests")
 
 def relative_to_assets(path: str) -> Path:
     return ASSETS_PATH / Path(path)
